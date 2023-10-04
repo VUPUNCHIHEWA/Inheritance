6a5f73168c16ec76f8aa49729fb7ef6263f2efc2
ASUS <40-bis-0023@kdu.ac.lk>
2023-10-04 16:47:59 +0530

    first commit

---
+++ src/Animal.java
@@ -0,0 +1,7 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+class Animal {
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal1 = new Dog();
+        Animal animal2 = new Cat();
+
+        animal1.makeSound(); // Output: Dog barks
+        animal2.makeSound(); // Output: Cat meows
+    }
+}
